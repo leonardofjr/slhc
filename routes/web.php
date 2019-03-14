f2e2170808935124305faf05bd19acce2a4566ab
Leo <leonardo.f.jr@gmail.com>
2019-03-13 23:47:56 -0400

    /

--- routes/web.php
+++ routes/web.php
@@ -56,14 +56,7 @@ Route::group(['middleware' => 'auth'], function () {
 
 
 
-// Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
