fd927776407fda48e69f6bec10780c7bda6c50ca
Leo <leonardo.f.jr@gmail.com>
2019-03-13 23:50:50 -0400

    /

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,6 @@ Route::group(['middleware' => 'auth'], function () {
 
 
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
