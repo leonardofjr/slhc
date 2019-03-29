52e354f0b9141b6ba72078dd8f2f80313ee070db
Leo <leonardo.f.jr@gmail.com>
2019-03-29 00:52:39 -0400

    ?

---
+++ app/DurationDropdown.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DurationDropdown extends Model
+{
+    //
+}
---
+++ database/seeds/DurationDropdownTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\DurationDropdown;
+
+class DurationDropdownTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
