73183762eaa60a9f3a6625babfef482bf509bc6b
Leo <hello@leojr.me>
2019-12-24 13:39:21 -0500

    /

--- resources/views/frontend/components/flip-cards.blade.php
+++ resources/views/frontend/components/flip-cards.blade.php
@@ -36,4 +36,3 @@
     @endforeach
   
 </div>
--->
--- resources/views/frontend/components/slideshow.blade.php
+++ resources/views/frontend/components/slideshow.blade.php
@@ -7,12 +7,27 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" src='{{asset("imgs/qi-gong.png")}}' alt="First slide">
+      <div class="center-carousel-content">
+          <h2>PAIN & INJURY</h2>
+          <p>Fast, Effective Results</p>
+          <button class="btn btn-primary">{{$company_phone}}</button> <button class="btn btn-teritary">BOOK NOW</button>
+      </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src='{{asset("imgs/meditation.png")}}' alt="Second slide">
+      <div class="center-carousel-content">
+        <h2>STRESS & ANXIETY</h2>
+        <p>Calm Your Mind and Body Down</p>
+        <button class="btn btn-primary">{{$company_phone}}</button> <button class="btn btn-teritary">BOOK NOW</button>
+    </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src='{{asset("imgs/yoga.png")}}' alt="Third slide">
+      <div class="center-carousel-content">
+        <h2>CHAKRA HEALING</h2>
+        <p>Keep Healthy, Happy and Vibrant</p>
+        <button class="btn btn-primary">{{$company_phone}}</button> <button class="btn btn-teritary">BOOK NOW</button>
+      </div>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
